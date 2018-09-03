405c9037187794ffce75a6153fda4a2c3f5efdfb
thy <doanthy.vo@gmail.com>
2018-09-03 19:43:07 -0400

    home page and survey page. app seems to be working

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Kermit',
+		photo: 'https://vignette.wikia.nocookie.net/jenna-marbles/images/3/3f/Th_%288%29.jpeg/revision/latest?cb=20180804061306',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Peach',
+		photo: 'https://vignette.wikia.nocookie.net/jenna-marbles/images/0/0c/Peach.jpg/revision/latest?cb=20171012135109',
+		scores: [
+			'4',
+			'4',
+			'1',
+			'3',
+			'3',
+			'3',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Marbles',
+		photo: 'https://pbs.twimg.com/media/DBl2ylpVwAAPpKH.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Klondike',
+		photo: 'https://i.redd.it/ywely2b0mya01.jpg',
+		scores: [
+			'5',
+			'1',
+			'3',
+			'3',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Sundae',
+		photo: 'https://pbs.twimg.com/media/C1gX-fRUQAAqGIQ.jpg',
+		scores: [
+			'1',
+			'3',
+			'4',
+			'4',
+			'5',
+			'1',
+			'1',
+			'3',
+			'5',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
